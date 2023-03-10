01f7e80cc4195a5d1c76ef3c697e0002ccea4cd6
SebasMtzHdez <sebas_godofwar16@hotmail.com>
2023-03-10 13:05:39 -0600

    alert

--- lib/presentation/screens/screen_of_screens.dart
+++ lib/presentation/screens/screen_of_screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:toma_datos_telmex/config/colors_app.dart';
 import 'package:toma_datos_telmex/presentation/screens/home_screen.dart';
 import '../widgets/widgets.dart';
 
@@ -35,6 +36,60 @@ class ScreenOfScreens extends StatelessWidget {
                     ),
                   ],
                 ),
+ElevatedButton(
+  onPressed: () {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Icon(
+                Icons.error,
+                size: 140,
+                color: Colors.red,
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Espera un momento', style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          content: const Text(
+            'Tienes algunas opciones sin seleccionar, por favor regresa y revisa tu formulario', style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          actions: [
+            Align(
+              alignment: Alignment.center,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                // ignore: sort_child_properties_last
+                child: const Text(
+                  'Cerrar',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: ColorsApp.primaryColor,
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  elevation: 5,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  },
+  child: const Text('alerta'),
+),
               ],
             );
           },
@@ -42,7 +97,6 @@ class ScreenOfScreens extends StatelessWidget {
       ),
     );
   }
-  
 }
 
 class StatusScreen {
