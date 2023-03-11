38f8682a3518500d108b64bf3abe313f084433c9
Fernando436 <montf36@gmail.com>
2023-03-11 11:24:08 -0600

    Fernando

--- lib/presentation/screens/pole_detail_screen.dart
+++ lib/presentation/screens/pole_detail_screen.dart
@@ -11,6 +11,9 @@ class PoleDetailScreen extends StatefulWidget {
 class _PoleDetailScreenState extends State<PoleDetailScreen> {
   // Variables para el menú desplegable numérico
   int _selectedValue = 0;
+  int _selectedValue2 = 0;
+  int _selectedValue3 = 0;
+
   final List<int> _dropdownValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   @override
